124fc89f670f3d58b296ab6ecc77441e2176c20c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-09-03 13:51:43 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+print('Sabor Express\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+opcao_escolhida = input('Escolha uma opção: ')
+
+print('Você escolheu a opção', opcao_escolhida)
